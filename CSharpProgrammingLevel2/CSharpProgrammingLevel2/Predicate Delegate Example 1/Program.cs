780d4e82f461e941c1cfb9f424315adcce7a9426
Abdullah Alhelal <abdullah.h@alameensoft.com>
2025-03-12 06:29:06 -0700

    Predicate Delegate

---
+++ CSharpProgrammingLevel2/CSharpProgrammingLevel2/Predicate Delegate Example 1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Predicate_Delegate_Example_1
+{
+    internal class Program
+    {
+
+        // Define a Func delegate for squaring a number
+        static Predicate<int> IsEvenPredicate = IsEven;
+
+        // Define a method that squares a number
+        static bool IsEven(int x)
+        {
+            return (x % 2 == 0);
+        }
+
+        static void Main()
+        {
+            // Use the IsEven Func to square the number 5
+            bool result = IsEvenPredicate(5);
+
+            // Print the result
+            Console.WriteLine("Is Number 5  Even ?  " + result);
+            Console.ReadKey();
+
+        }
+    }
+
+}
---
+++ CSharpProgrammingLevel2/CSharpProgrammingLevel2/Predicate Delegate Example 1/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Predicate Delegate Example 1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Predicate Delegate Example 1")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f895827a-4295-4c86-91e7-796126e1eb10")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
