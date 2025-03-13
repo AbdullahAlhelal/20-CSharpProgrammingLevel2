d384cd101c8945529dec718f7479d6d6b9a8e884
Abdallh Alhelel <abdallh20hl@gmail.com>
2025-03-13 20:04:28 +0300

    Attributes

---
+++ CSharpProgrammingLevel2/CSharpProgrammingLevel2/Attributes/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace Attributes
+{
+    internal class Program
+    {
+
+        [Obsolete("This method is deprecated. Use NewMethod instead.")]
+        public void DeprecatedMethod()
+        {
+            // Deprecated method implementation
+        }
+
+
+        [Conditional("DEBUG")]
+        public void DebugMethod()
+        {
+            // Code to be executed only in debug mode
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
