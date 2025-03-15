b808baf5da4a77e7f6d5bfa6ce5e4de57e5bcb10
Abdallh Alhelel <abdallh20hl@gmail.com>
2025-03-15 22:49:45 +0300

    Reading from Registry

--- CSharpProgrammingLevel2/CSharpProgrammingLevel2/Registry in C#/Program.cs
+++ CSharpProgrammingLevel2/CSharpProgrammingLevel2/Registry in C#/Program.cs
@@ -31,6 +31,27 @@ namespace Registry_in_C_
             {
                 Console.WriteLine($"An error occurred: {ex.Message}");
             }
+
+
+            try
+            {
+                // Read the value from the Registry
+                string value = Registry.GetValue(keyPath , valueName , null) as string;
+
+
+                if ( value != null )
+                {
+                    Console.WriteLine($"The value of {valueName} is: {value}");
+                }
+                else
+                {
+                    Console.WriteLine($"Value {valueName} not found in the Registry.");
+                }
+            }
+            catch ( Exception ex )
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+            }
         }
     }
 }
