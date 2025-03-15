340898096c1f831f2b2660dcfd85fce636a61975
Abdallh Alhelel <abdallh20hl@gmail.com>
2025-03-15 23:28:59 +0300

    begin Event Log   Logging Example

---
+++ CSharpProgrammingLevel2/CSharpProgrammingLevel2/Event Log/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Event_Log
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Specify the source name for the event log
+            string sourceName = "MyApp";
+
+
+            // Create the event source if it does not exist
+            if ( !EventLog.SourceExists(sourceName) )
+            {
+                EventLog.CreateEventSource(sourceName , "Application");
+                Console.WriteLine("Event source created.");
+            }
+
+
+            // Log an information event
+            EventLog.WriteEntry(sourceName , "This is an information event." , EventLogEntryType.Information);
+            EventLog.WriteEntry(sourceName , "This is an information Warning event." , EventLogEntryType.Warning);
+            EventLog.WriteEntry(sourceName , "This is an information Error event." , EventLogEntryType.Error);
+
+
+            Console.WriteLine("Event written to the log.");
+        }
+    }
+    }
+
