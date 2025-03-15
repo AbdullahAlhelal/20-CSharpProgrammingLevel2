e4e9606949179666600e4606b6fcd33b54d9b45b
Abdallh Alhelel <abdallh20hl@gmail.com>
2025-03-15 23:15:20 +0300

    DeleteValue from Registry

--- CSharpProgrammingLevel2/CSharpProgrammingLevel2/Registry in C#/Program.cs
+++ CSharpProgrammingLevel2/CSharpProgrammingLevel2/Registry in C#/Program.cs
@@ -52,6 +52,44 @@ namespace Registry_in_C_
             {
                 Console.WriteLine($"An error occurred: {ex.Message}");
             }
+
+            //Delete 
+
+            try
+            {
+                // Open the registry key in read/write mode with explicit registry view
+                using ( RegistryKey baseKey = RegistryKey.OpenBaseKey(RegistryHive.CurrentUser , RegistryView.Registry64) )
+                {
+                    using ( RegistryKey key = baseKey.OpenSubKey(keyPath , true) )
+                    {
+                        if ( key != null )
+                        {
+                            // Delete the specified value
+                            key.DeleteValue(valueName);
+
+
+                            Console.WriteLine($"Successfully deleted value '{valueName}' from registry key '{keyPath}'");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Registry key '{keyPath}' not found");
+                        }
+                    }
+                }
+            }
+            catch ( UnauthorizedAccessException )
+            {
+                Console.WriteLine("UnauthorizedAccessException: Run the program with administrative privileges.");
+            }
+            catch ( Exception ex )
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+            }
+
+
+            Console.ReadKey();
         }
+
     }
+    
 }
