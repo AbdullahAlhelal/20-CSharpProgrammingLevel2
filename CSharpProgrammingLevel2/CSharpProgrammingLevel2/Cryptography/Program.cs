faebaba422887dd4a61346217462ffe123c4ddf2
Abdallh Alhelel <abdallh20hl@gmail.com>
2025-03-16 21:49:09 +0300

    ASymmetricEncryption EncryptandDecryptImage

--- CSharpProgrammingLevel2/CSharpProgrammingLevel2/Cryptography/Program.cs
+++ CSharpProgrammingLevel2/CSharpProgrammingLevel2/Cryptography/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
@@ -29,7 +30,7 @@ namespace Cryptography
             }
         }
 
-        #region 
+        #region SymmetricEncryption
 
         public static void SymmetricEncryption()
 
@@ -110,6 +111,185 @@ namespace Cryptography
         }
 
         #endregion
+
+   
+        #region ASymmetricEncryption
+        public static void ASymmetricEncryption()
+        {
+            try
+            {
+                // Generate public and private key pair
+                using ( RSACryptoServiceProvider rsa = new RSACryptoServiceProvider() )
+                {
+                    // Get the public key
+                    /*
+                     When exporting the public key, ToXmlString(false) is used with the argument set 
+                     to false to indicate that only the public parameters should be included in the XML string.
+                     */
+                    string publicKey = rsa.ToXmlString(false);
+
+
+                    // Get the private key
+                    string privateKey = rsa.ToXmlString(true);
+
+
+                    // Original message
+                    string originalMessage = "Hello, this is a secret message!";
+
+
+                    // Encrypt using the public key
+                    string encryptedMessage = AsEncrypt(originalMessage , publicKey);
+
+
+                    // Decrypt using the private key
+                    string decryptedMessage = AsDecrypt(encryptedMessage , privateKey);
+
+
+                    // Display the results
+                    Console.WriteLine($"\n\nPublic Key:\n {publicKey}");
+                    Console.WriteLine($"\n\nPrivate Key:\n {privateKey}");
+                    Console.WriteLine($"\nOriginal Message:\n {originalMessage}");
+                    Console.WriteLine($"\nEncrypted Message:\n {encryptedMessage}");
+                    Console.WriteLine($"\nDecrypted Message:\n {decryptedMessage}");
+
+
+                    // Wait for user input before closing the console window
+                    Console.WriteLine("\nPress any key to exit...");
+                    Console.ReadKey();
+                }
+            }
+            catch ( CryptographicException ex )
+            {
+                Console.WriteLine($"Encryption/Decryption error: {ex.Message}");
+                Console.ReadKey();
+            }
+            catch ( Exception ex )
+            {
+                Console.WriteLine($"An unexpected error occurred: {ex.Message}");
+                Console.ReadKey();
+            }
+        }
+
+
+        static string AsEncrypt(string plainText , string publicKey)
+        {
+            try
+            {
+                using ( RSACryptoServiceProvider rsa = new RSACryptoServiceProvider() )
+                {
+                    rsa.FromXmlString(publicKey);
+
+
+                    byte[] encryptedData = rsa.Encrypt(Encoding.UTF8.GetBytes(plainText) , false);
+                    return Convert.ToBase64String(encryptedData);
+                }
+            }
+            catch ( CryptographicException ex )
+            {
+                Console.WriteLine($"Encryption error: {ex.Message}");
+                throw; // Rethrow the exception to be caught in the Main method
+            }
+        }
+
+
+        public static string AsDecrypt(string cipherText , string privateKey)
+        {
+            try
+            {
+                using ( RSACryptoServiceProvider rsa = new RSACryptoServiceProvider() )
+                {
+                    rsa.FromXmlString(privateKey);
+
+
+                    byte[] encryptedData = Convert.FromBase64String(cipherText);
+                    byte[] decryptedData = rsa.Decrypt(encryptedData , false);
+
+
+                    return Encoding.UTF8.GetString(decryptedData);
+                }
+            }
+            catch ( CryptographicException ex )
+            {
+                Console.WriteLine($"Decryption error: {ex.Message}");
+                throw; // Rethrow the exception to be caught in the Main method
+            }
+        }
+        #endregion
+
+        #region Encrypt and Decrypt Image
+
+        public static void EncryptandDecryptImage()
+        {
+
+            string inputFile = "c:\\Image\\MyImage.jpg";
+            string encryptedFile = "c:\\Image\\encrypted.jpg";
+            string decryptedFile = "c:\\Image\\decrypted.jpg";
+
+
+            // Generate a random IV for each encryption operation
+            byte[] iv;
+            using ( Aes aesAlg = Aes.Create() )
+            {
+                iv = aesAlg.IV;
+            }
+
+
+            string key = "1234567890123456";
+
+
+            EncryptFile(inputFile , encryptedFile , key , iv);
+            DecryptFile(encryptedFile , decryptedFile , key , iv);
+
+
+            Console.WriteLine("Encryption and decryption completed successfully.");
+            Console.WriteLine("go to c:\\Image folder to see the results");
+            Console.ReadKey();
+
+        }
+
+        static void EncryptFile(string inputFile , string outputFile , string key , byte[] iv)
+        {
+            using ( Aes aesAlg = Aes.Create() )
+            {
+                aesAlg.Key = System.Text.Encoding.UTF8.GetBytes(key);
+                aesAlg.IV = iv;
+
+
+                using ( FileStream fsInput = new FileStream(inputFile , FileMode.Open) )
+                using ( FileStream fsOutput = new FileStream(outputFile , FileMode.Create) )
+                using ( ICryptoTransform encryptor = aesAlg.CreateEncryptor() )
+                using ( CryptoStream cryptoStream = new CryptoStream(fsOutput , encryptor , CryptoStreamMode.Write) )
+                {
+                    // Write the IV to the beginning of the file
+                    fsOutput.Write(iv , 0 , iv.Length);
+                    fsInput.CopyTo(cryptoStream);
+                }
+            }
+        }
+
+
+        static void DecryptFile(string inputFile , string outputFile , string key , byte[] iv)
+        {
+            using ( Aes aesAlg = Aes.Create() )
+            {
+                aesAlg.Key = System.Text.Encoding.UTF8.GetBytes(key);
+                aesAlg.IV = iv;
+
+
+                using ( FileStream fsInput = new FileStream(inputFile , FileMode.Open) )
+                using ( FileStream fsOutput = new FileStream(outputFile , FileMode.Create) )
+                using ( ICryptoTransform decryptor = aesAlg.CreateDecryptor() )
+                using ( CryptoStream cryptoStream = new CryptoStream(fsOutput , decryptor , CryptoStreamMode.Write) )
+                {
+                    // Skip the IV at the beginning of the file
+                    fsInput.Seek(iv.Length , SeekOrigin.Begin);
+                    fsInput.CopyTo(cryptoStream);
+                }
+            }
+        }
+    
+        #endregion
+
         static void Main(string[] args)
         {
 
@@ -127,6 +307,8 @@ namespace Cryptography
 
             SymmetricEncryption();
             // Pause to keep the console window open for viewing the results
+            Console.ReadKey();
+            ASymmetricEncryption();
             Console.ReadKey();
         }
     }
