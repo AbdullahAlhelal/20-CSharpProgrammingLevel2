6ff18d948212a6916243d13dade01e0af041c060
Abdallh Alhelel <abdallh20hl@gmail.com>
2025-03-16 21:27:20 +0300

    Symmetric Encryption encript decript

--- CSharpProgrammingLevel2/CSharpProgrammingLevel2/Cryptography/Program.cs
+++ CSharpProgrammingLevel2/CSharpProgrammingLevel2/Cryptography/Program.cs
@@ -5,6 +5,7 @@ using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace Cryptography
 {
 
@@ -17,18 +18,98 @@ namespace Cryptography
         {
             //SHA is Secutred Hash Algorithm.
             // Create an instance of the SHA-256 algorithm
-            using (SHA256 sha256 = SHA256.Create())
+            using ( SHA256 sha256 = SHA256.Create() )
             {
                 // Compute the hash value from the UTF-8 encoded input string
                 byte[] hashBytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(input));
 
 
                 // Convert the byte array to a lowercase hexadecimal string
-                return BitConverter.ToString(hashBytes).Replace("-", "").ToLower();
+                return BitConverter.ToString(hashBytes).Replace("-" , "").ToLower();
+            }
+        }
+
+        #region 
+
+        public static void SymmetricEncryption()
+
+        {
+            // Original data
+            string originalData = "Sensitive information";
+
+
+            // Key for AES encryption (128-bit key)
+            string key = "1234567890123456";
+
+
+            // Encrypt the original data
+            string encryptedData = Encrypt(originalData , key);
+
+
+            // Decrypt the encrypted data
+            string decryptedData = Decrypt(encryptedData , key);
+
+
+            // Display results
+            Console.WriteLine($"Original Data: {originalData}");
+            Console.WriteLine($"Encrypted Data: {encryptedData}");
+            Console.WriteLine($"Decrypted Data: {decryptedData}");
+        }
+        static string Encrypt(string plainText , string key)
+        {
+            using ( Aes aesAlg = Aes.Create() )
+            {
+                // Set the key and IV for AES encryption
+                aesAlg.Key = Encoding.UTF8.GetBytes(key);
+                aesAlg.IV = new byte[aesAlg.BlockSize / 8];
+
+
+                // Create an encryptor
+                ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key , aesAlg.IV);
+
+
+                // Encrypt the data
+                using ( var msEncrypt = new System.IO.MemoryStream() )
+                {
+                    using ( var csEncrypt = new CryptoStream(msEncrypt , encryptor , CryptoStreamMode.Write) )
+                    using ( var swEncrypt = new System.IO.StreamWriter(csEncrypt) )
+                    {
+                        swEncrypt.Write(plainText);
+                    }
+
+
+                    // Return the encrypted data as a Base64-encoded string
+                    return Convert.ToBase64String(msEncrypt.ToArray());
+                }
             }
         }
 
 
+        static string Decrypt(string cipherText , string key)
+        {
+            using ( Aes aesAlg = Aes.Create() )
+            {
+                // Set the key and IV for AES decryption
+                aesAlg.Key = Encoding.UTF8.GetBytes(key);
+                aesAlg.IV = new byte[aesAlg.BlockSize / 8];
+
+
+                // Create a decryptor
+                ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key , aesAlg.IV);
+
+
+                // Decrypt the data
+                using ( var msDecrypt = new System.IO.MemoryStream(Convert.FromBase64String(cipherText)) )
+                using ( var csDecrypt = new CryptoStream(msDecrypt , decryptor , CryptoStreamMode.Read) )
+                using ( var srDecrypt = new System.IO.StreamReader(csDecrypt) )
+                {
+                    // Read the decrypted data from the StreamReader
+                    return srDecrypt.ReadToEnd();
+                }
+            }
+        }
+
+        #endregion
         static void Main(string[] args)
         {
 
@@ -44,7 +125,7 @@ namespace Cryptography
             Console.WriteLine($"Original Data: {data}");
             Console.WriteLine($"Hashed Data: {hashedData}");
 
-
+            SymmetricEncryption();
             // Pause to keep the console window open for viewing the results
             Console.ReadKey();
         }
