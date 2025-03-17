1f0dbb38d92ca68890fb0b9bdfee661c5e20b7f6
Abdallh Alhelel <abdallh20hl@gmail.com>
2025-03-17 21:47:27 +0300

    Parallel Class Parallel.For

---
+++ CSharpProgrammingLevel2/CSharpProgrammingLevel2/Parallel Class/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parallel_Class
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //  Parallel.For
+
+            // Define the number of iterationsy
+            int numberOfIterations = 10;
+
+
+            // Use Parallel.For to execute the loop in parallel
+            Parallel.For(0 , numberOfIterations , i =>
+            {
+                Console.WriteLine($"Executing iteration {i} on thread {Task.CurrentId}");
+                // Simulate some work
+                Task.Delay(1000).Wait();
+            });
+
+        }
+    }
+}
