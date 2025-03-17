56fb6a829117d23da471526b5bed0eec67fede56
Abdullah Alhelal <abdullah.h@alameensoft.com>
2025-03-17 02:39:02 -0700

    Synchronization Example with use lock

--- CSharpProgrammingLevel2/CSharpProgrammingLevel2/Process vs Thread/Program.cs
+++ CSharpProgrammingLevel2/CSharpProgrammingLevel2/Process vs Thread/Program.cs
@@ -7,8 +7,38 @@ namespace Process_vs_Thread
 {
     internal class Program
     {
-        static void ParameterizedThread( ) 
+        static int sharedCounter = 0;
+        static object lockObject = new object();
+        static void IncrementCounter()
         {
+            for (int i = 0; i < 100000; i++)
+            {
+                // Use lock to synchronize access to the shared counter
+                lock (lockObject)
+                {
+                    sharedCounter++;
+                }
+            }
+        }
+        static void SynchronizationExample( ) 
+        {
+            
+            // Create two threads that increment a shared counter
+            Thread t1 = new Thread(IncrementCounter);
+            Thread t2 = new Thread(IncrementCounter);
+
+
+            t1.Start();
+            t2.Start();
+
+
+            // Wait for both threads to complete
+            t1.Join();
+            t2.Join();
+
+
+            Console.WriteLine("Final Counter Value: " + sharedCounter);
+            Console.ReadKey();
         }
 
 
