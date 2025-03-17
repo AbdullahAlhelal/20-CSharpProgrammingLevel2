4d5138c937dd0277b3349eeb3cb0dbe25e9647f0
Abdallh Alhelel <abdallh20hl@gmail.com>
2025-03-17 21:53:19 +0300

    Parallel.ForEach

--- CSharpProgrammingLevel2/CSharpProgrammingLevel2/Parallel Class/Program.cs
+++ CSharpProgrammingLevel2/CSharpProgrammingLevel2/Parallel Class/Program.cs
@@ -1,13 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Parallel_Class
 {
     internal class Program
     {
+
+        static List<string> urls = new List<string>
+    {
+        "https://www.cnn.com",
+        "https://www.amazon.com",
+        "https://www.programmingadvices.com"
+    };
+
+        static void DownloadContent(string url)
+        {
+            string content;
+
+
+            using ( WebClient client = new WebClient() )
+            {
+                // Simulate some work by adding a delay
+                Thread.Sleep(100);
+
+
+                // Download the content of the web page
+                content = client.DownloadString(url);
+            }
+
+
+            Console.WriteLine($"{url}: {content.Length} characters downloaded");
+        }
+
         static void Main(string[] args)
         {
             //  Parallel.For
@@ -24,6 +53,22 @@ namespace Parallel_Class
                 Task.Delay(1000).Wait();
             });
 
+            //Parallel.ForEach
+
+
+
+            // Use Parallel.ForEach to download the web pages concurrently
+            Parallel.ForEach(urls , url =>
+            {
+                DownloadContent(url);
+
+            });
+
+
+            Console.WriteLine("Done!");
+            Console.ReadKey();
         }
+
+
     }
 }
