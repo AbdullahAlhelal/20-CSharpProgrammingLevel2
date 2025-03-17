e17509249aa7660aa7f176c14e57068822deb88c
Abdullah Alhelal <abdullah.h@alameensoft.com>
2025-03-17 02:46:29 -0700

    Calling a SP_AddNewPerson from C# Using ADO.NET

---
+++ CSharpProgrammingLevel2/CSharpProgrammingLevel2/StoredProcdure/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoredProcdure
+{
+    //Calling a SP_AddNewPerson from C# Using ADO.NET
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = "YourConnectionStringHere";
+            SqlConnection connection = new SqlConnection(connectionString);
+            SqlCommand command = new SqlCommand("SP_AddNewPerson", connection);
+            command.CommandType = CommandType.StoredProcedure;
+
+
+            // Add parameters
+            command.Parameters.AddWithValue("@FirstName", "John");
+            command.Parameters.AddWithValue("@LastName", "Doe");
+            command.Parameters.AddWithValue("@Email", "john.doe@example.com");
+            SqlParameter outputIdParam = new SqlParameter("@NewPersonID", SqlDbType.Int)
+            {
+                Direction = ParameterDirection.Output
+            };
+            command.Parameters.Add(outputIdParam);
+
+
+            // Execute
+            connection.Open();
+            command.ExecuteNonQuery();
+
+
+            // Retrieve the ID of the new person
+            int newPersonID = (int)command.Parameters["@NewPersonID"].Value;
+            Console.WriteLine($"New Person ID: {newPersonID}");
+
+
+            connection.Close();
+        }
+    }
+}
